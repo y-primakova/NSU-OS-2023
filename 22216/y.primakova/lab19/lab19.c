b2bcb83e216c2a18cba6fd354306d0c2a289792e
Yulia Primakova <y.primakova@g.nsu.ru>
2024-09-03 19:54:15 +0700

    fix

--- 22216/y.primakova/lab19/lab19.c
+++ 22216/y.primakova/lab19/lab19.c
@@ -8,36 +8,36 @@
 #include <errno.h>
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		fprintf(stderr, "Argc error.");
-		return -1;
-	}
-	if (strchr(argv[1], '/') != NULL) {
-		fprintf(stderr, "Pattern contains '/'.");
-		return -1;
-	}
-	DIR* dir;
-	if (!(dir = opendir("."))) {
-		perror("Opendir failed.");
-		return -1;
-	}
-	struct dirent* entry;
-	int count = 0;
-	errno = 0;
-	while ((entry = readdir(dir)) != NULL) {
-		if (fnmatch(argv[1], entry->d_name, FNM_PATHNAME) == 0) {
-			printf("%s\n", entry->d_name);
-			count++;
-		}
-	}
-	if (errno != 0) {
-		perror("Readdir failed.");
-		closedir(dir);
-		return -1;
-	}
-	if (count == 0) {
-		printf("%s", argv[1]);
-	}
-	closedir(dir);
-	return 0;
+    if (argc != 2) {
+        fprintf(stderr, "There is no pattern.");
+        exit(-1);
+    }
+    if (strchr(argv[1], '/') != NULL) {
+        fprintf(stderr, "Pattern contains '/'.");
+        exit(-1);
+    }
+    DIR* dir;
+    if (!(dir = opendir("."))) {
+        perror("Opendir fail.");
+        exit(-1);
+    }
+    struct dirent* entry;
+    int count = 0;
+    errno = 0;
+    while ((entry = readdir(dir)) != NULL) {
+        if (fnmatch(argv[1], entry->d_name, FNM_PATHNAME) == 0) {
+            printf("%s\n", entry->d_name);
+            count++;
+        }
+    }
+    if (errno != 0) {
+        perror("Readdir fail.");
+        closedir(dir);
+        exit(-1);
+    }
+    if (count == 0) {
+        printf("%s", argv[1]);
+    }
+    closedir(dir);
+    return 0;
 }
