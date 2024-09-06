3d5e92af8f410f623b7d9aa894d6e2877168b34b
Yulia Primakova <y.primakova@g.nsu.ru>
2024-09-07 00:14:27 +0700

    lab10

---
+++ 22216/y.primakova/lab10/lab10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "There is no command.");
+        exit(-1);
+    }
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Fork fail.");
+        exit(-1);
+    } 
+    if (pid == 0) {
+        execvp(argv[1], &argv[1]);
+        perror("Execvp fail.");
+        exit(-1);
+    } 
+    int status;
+    if (wait(&status) == -1) {
+        perror("Wait fail.");
+        exit(-1);
+    }
+    if (WIFEXITED(status)) {
+        printf("The child terminated normally. The exit status of the child: %d.", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        printf("The child process was terminated by a signal. The number of the signal that caused the child process to terminate: %d.", WTERMSIG(status));
+    }
+    return 0;
+}
