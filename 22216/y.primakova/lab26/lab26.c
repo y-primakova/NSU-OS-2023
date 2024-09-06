f2ae4fff42169c4234db3db1c345e71cb4d96db5
Yulia Primakova <y.primakova@g.nsu.ru>
2024-09-06 18:40:01 +0700

    read from file

--- 22216/y.primakova/lab26/lab26.c
+++ 22216/y.primakova/lab26/lab26.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of arguments.");
+        return -1;
+    }
     char buff[BUFSIZ];
-    FILE *fpin = popen("echo Hello, World!", "r");
+    FILE *fpin = fopen(argv[1], "r");
     if (fpin == NULL) {
-        perror("Popen failed.");
+        perror("Fopen failed.");
         return -1;
     }
     FILE *fpout = popen("tr [:lower:] [:upper:]", "w");
     if (fpout == NULL) {
         perror("Popen failed.");
-        pclose(fpin);
+        fclose(fpin);
         return -1;
     }
     while (fgets(buff, BUFSIZ, fpin) != NULL) {
         if (fputs(buff, fpout) == EOF) {
             perror("Fputs failed.");
-            pclose(fpin);
+            fclose(fpin);
             pclose(fpout);
             return -1;
         }
     }
-    pclose(fpin);
+    fclose(fpin);
     pclose(fpout);
     return 0;
 }
